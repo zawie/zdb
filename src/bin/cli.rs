9c7417919eb71cee351941edead9e9bcb6abbba2
zawie <zawie@rice.edu>
2024-06-23 13:16:13 -0700

    Simple map based implementation

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,13 +1,53 @@
 use lib::database;
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
 
-    let db = database::Database::new();
+    let mut db = database::Database::new();
 
-    let key = vec![1, 2, 3];
-    let value = vec![4, 5, 6];
+    loop {
+        let mut input = String::new();
 
-    db.set(&key, &value)
-        .expect("Failed to set!");    
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let chunks: Vec<&str> = input
+            .trim()
+            .split(" ")
+            .collect();
+
+        match chunks[0] {
+            "get" => {
+                if chunks.len() != 2 {
+                    println!("Usage: get <key>");
+                    continue;
+                }
+                let r = db.get(&chunks[1].to_string());
+                match r {
+                    Ok(_) => {
+                        match r.unwrap() {
+                            Some(value) => println!("Value: {}", value),
+                            None => println!("Key not found!")
+                        }
+                    }
+                    Err(e) => println!("Error setting value: {}", e)
+                }
+            }
+            "set" => {
+                if chunks.len() != 3 {
+                    println!("Usage: set <key> <value>");
+                    continue;
+                }
+                let r = db.set(chunks[1].to_string(), chunks[2].to_string());
+                match r {
+                    Ok(_) => println!("Value set!"),
+                    Err(e) => println!("Error setting value: {}", e)
+                }
+            }
+            _ => {
+                println!("Unknown command! Known commands: get <key>, set <key>");
+            }
+        }
+    }
 }
--- src/database.rs
+++ src/database.rs
@@ -1,23 +1,33 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
+
 
 type SetResult = std::result::Result<(), Box<dyn Error>>;
-type GetResult<T> = std::result::Result<T, Box<dyn Error>>;
+type GetResult = std::result::Result<Option<String>, Box<dyn Error>>;
 
 pub struct Database {
+    map: HashMap<String, String>
 }
+
+
 
 impl Database {
     pub fn new() -> Database {
-        Database {}
+        Database {
+            map: HashMap::new()
+        }
     }
 
-    pub fn set(&self, key: &Vec<u8>, value: &Vec<u8>) -> SetResult {
+    pub fn set(&mut self, key: String, value: String) -> SetResult {
+        self.map.insert(key, value);
 
-        return Err("Not implemented yet!".into())
+        return Ok(())
     }
 
-    pub fn get(&self, key: &Vec<u8>) -> GetResult<Vec<u8>> {
-    
-        return Err("Not implemented yet!".into())
+    pub fn get(&self, key: &str) -> GetResult {
+        
+        match self.map.get(key) {
+            Some(value) => Ok(Some(value.clone())),
+            None => Ok(None)
+        }
     }
 }
