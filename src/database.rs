169eb5e1e2a29c7a38afec5a783075b7a9f53f21
zawie <zawie@rice.edu>
2024-06-23 13:27:20 -0700

    Track memory usage

--- src/database.rs
+++ src/database.rs
@@ -5,7 +5,8 @@ type SetResult = std::result::Result<(), Box<dyn Error>>;
 type GetResult = std::result::Result<Option<String>, Box<dyn Error>>;
 
 pub struct Database {
-    map: HashMap<String, String>
+    map: HashMap<String, String>,
+    memory_usage: usize
 }
 
 
@@ -13,18 +14,23 @@ pub struct Database {
 impl Database {
     pub fn new() -> Database {
         Database {
-            map: HashMap::new()
+            map: HashMap::new(),
+            memory_usage: 0,
         }
     }
 
     pub fn set(&mut self, key: String, value: String) -> SetResult {
-        self.map.insert(key, value);
-
+        self.memory_usage += key.len() + value.len();
+        match self.map.insert(key, value) {
+            Some(v) => {
+                self.memory_usage -= v.len();
+            }
+            None => {}
+        }
         return Ok(())
     }
 
     pub fn get(&self, key: &str) -> GetResult {
-        
         match self.map.get(key) {
             Some(value) => Ok(Some(value.clone())),
             None => Ok(None)
