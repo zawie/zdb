9424206f64c403f79f4e67c22f47d5dfe138ebed
zawie <zawie@rice.edu>
2024-07-06 18:02:28 -0700

    🧱 Implements working segment

--- src/segment_store.rs
+++ src/segment_store.rs
@@ -1,9 +1,11 @@
-use std::{error::Error, fs::{File, OpenOptions}, io::{self, BufRead, BufReader, Cursor, Read, Seek, Write}};
-
-use std::mem::transmute;
+use std::{error::Error, fs::{File, OpenOptions}, io::{self, BufRead, BufReader, Read, Seek, Write}};
+use std::str;
 
 use super::{GetResult};
 
+use log::{debug};
+
+const BLOCK_SIZE: usize = 10000;
 pub struct SegmentStore {
     file_path: String,  
     index: Vec<(String, usize)>, // (key, offset)
@@ -17,7 +19,7 @@ impl SegmentStore {
 
         let mut index = Vec::new();
 
-        let mut buffer = String::new();
+        let mut buffer = Vec::new();
         let mut first_key: Option<String> = None;
 
         for (k, v) in sorted_iterator {
@@ -26,12 +28,13 @@ impl SegmentStore {
                 first_key = Some(k.to_string());
             }
 
-            buffer.push_str(&encode_string(k.as_str())?);
-            buffer.push_str(&encode_string(v.as_str())?);
+            buffer.extend(&encode(k.as_bytes())?);
+            buffer.extend(&encode(v.as_bytes())?);
 
-            if buffer.len() > 1000 {
-                bytes_written += writer.write(encode_string(first_key.unwrap().as_str())?.as_bytes())?;
-                bytes_written += writer.write(encode_string(&compress(&buffer))?.as_bytes())?;
+            if buffer.len() > BLOCK_SIZE {
+                debug!("Writing block of size {} with first key \"{}\"", buffer.len(), first_key.clone().unwrap());
+                bytes_written += writer.write(encode(first_key.unwrap().as_bytes())?.as_slice())?;
+                bytes_written += writer.write(encode(compress(&buffer))?.as_slice())?;
                 
                 buffer.clear();
                 first_key = None;
@@ -39,9 +42,12 @@ impl SegmentStore {
         }
 
         if buffer.len() > 0 {
-            writer.write(encode_string(first_key.unwrap().as_str())?.as_bytes())?;
-            writer.write(encode_string(&compress(&buffer))?.as_bytes())?;
+            debug!("Writing final block of size {} with first key \"{}\"", buffer.len(), first_key.clone().unwrap());
+            bytes_written += writer.write(encode(first_key.unwrap().as_bytes())?.as_slice())?;
+            bytes_written += writer.write(encode(compress(&buffer))?.as_slice())?;
         }
+
+        debug!("Finished writing segment to {}. Wrote {} bytes in {} blocks", file_path, bytes_written, index.len());
 
         Ok(SegmentStore{
             file_path: file_path,
@@ -53,34 +59,35 @@ impl SegmentStore {
 impl SegmentStore {
 
     pub fn get(&self, key: &str) -> GetResult {
+        debug!("Getting key from segment");
+
         let key = key.to_string();
 
-        let mut low = 0;
-        let mut high = self.index.len() - 1;
+        let (block_key, offset) = match closest_element_before(key.clone(), &self.index) {
+            Some(k) => k,
+            None => return Ok(None),
+        };
 
-        while low <= high {
-            let mid = (low + high) / 2;
-            let (mid_key, offset) = &self.index[mid];
+        debug!("Nearest key for \"{}\" is \"{}\" in segment {}", key, block_key, self.file_path);
+        debug!("Reading from offset {} in {}", offset, self.file_path);
 
-            if *mid_key == key {
-                // Key found, read the corresponding value from the file
-                let mut reader = self.start_from_offset(*offset)?;
-                let (_, block) = read_entry(&mut reader)?;
+        let mut reader = self.start_from_offset(offset)?;
+        let (_, block) = read_entry(&mut reader)?;
 
-                let data = decompress(&block);
-                let mut reader: BufReader<&[u8]> = BufReader::new(data.as_bytes());
-                while !reader.fill_buf()?.is_empty() {
-                    let (k, v) = read_entry(&mut reader)?;
-                    if k == key {
-                        return Ok(Some(v));
+        debug!("Block size: {}", block.len());
+
+
+        let data = decompress(&block);
+        let mut reader: BufReader<&[u8]> = BufReader::new(data);
+        while !reader.fill_buf()?.is_empty() {
+            let (k, v) = read_entry(&mut reader)?;
+            if k == key.as_bytes() {
+                return Ok(Some(
+                    match str::from_utf8(v.as_slice()) {
+                        Ok(v) => String::from(v),
+                        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
                     }
-                }
-
-                return Ok(None);
-            } else if *mid_key < key {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+                ));
             }
         }
 
@@ -97,33 +104,68 @@ impl SegmentStore {
 
 }
 
-fn read_entry(reader: &mut impl Read) -> Result<(String, String), Box<dyn Error>> {
-    let key = decode_string(reader)?;
-    let value = decode_string(reader)?;
+fn closest_element_before<K:PartialOrd + Clone, V: Clone> (key: K, elements: &Vec<(K,V)>) -> Option<(K,V)> {
+    if elements.len() == 0 {
+        return None;
+    }
+
+    let mut low = 0;
+    let mut high = elements.len() - 1;
+
+    let mut mid: usize = 0;
+    while low <= high {
+        mid = (low + high) / 2;
+        let mid_key = &elements[mid].0;
+
+        if *mid_key == key {
+            break;
+        } else if *mid_key < key {
+            low = mid + 1;
+        } else {
+            if mid == 0 {
+                break;
+            }
+            high = mid - 1;
+        }
+    }
+
+    if elements[mid].0 > key {
+        if mid == 0 {
+            return None;
+        }            
+        mid -= 1;
+    }
+    return Some(elements[mid].clone());
+}
+
+fn read_entry(reader: &mut impl Read) -> Result<(Vec<u8>, Vec<u8>,), Box<dyn Error>> {
+    let key = decode(reader)?;
+    let value = decode(reader)?;
 
     Ok((key, value))
 }
 
-fn decode_string(reader: &mut impl Read) -> Result<String, Box<dyn Error>> {
-    let mut len_bytes = [0u8; 8];
-    reader.read_exact(&mut len_bytes)?;
+fn decode(reader: &mut impl Read) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut buffer = [0u8; 8];
+    reader.read_exact(&mut buffer)?;
 
-    let len = usize::from_ne_bytes(len_bytes);
-
+    let len = usize::from_ne_bytes(buffer);
+    debug!("Reading string of length {}", len);
     let mut buffer = vec![0u8; len];
     reader.read_exact(&mut buffer)?;
 
-    Ok(String::from_utf8_lossy(&buffer).to_string())
+    Ok(buffer)
 }
 
-fn encode_string(input_string: &str) -> Result<String, Box<dyn Error>> {
-    let mut entry = String::new();
+fn encode(input_string: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut entry = Vec::new();
 
     let len_bytes: [u8; 8] = input_string.len().to_ne_bytes();
-    let len_string = String::from_utf8_lossy(&len_bytes).to_string();
 
-    entry.push_str(&len_string);
-    entry.push_str(input_string);
+    assert_eq!(usize::from_ne_bytes(len_bytes), input_string.len(), "Length mismatch");
+
+    entry.extend_from_slice(&len_bytes);
+    entry.extend_from_slice(input_string);
 
     Ok(entry)
 }
@@ -137,10 +179,81 @@ fn get_writer(file_path: &str) -> File {
         .unwrap()
 }
 
-fn compress(input: &String) -> String {
-    input.to_owned()
+fn compress(input: &[u8]) -> &[u8] {
+    input
 }
 
-fn decompress(input: &String) -> String {
-    input.to_owned()
+fn decompress(input: &[u8]) -> &[u8] {
+    input
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::BTreeMap, fs};
+
+    use super::*;
+    use rand::{distributions::Alphanumeric, Rng}; // 0.8
+
+    #[test]
+    fn test_closest_element_before() {
+        let elements: Vec<(&str,())> = [].to_vec();
+        assert_eq!(closest_element_before("Oh no!", &elements), None);
+
+        let elements = [("b", ()), ("c", ()), ("d",()), ("e",()), ("f",()), ("g",()), ("h",()), ("i",())].to_vec();
+         assert_eq!(closest_element_before("a", &elements), None);
+         assert_eq!(closest_element_before("b", &elements).unwrap(), ("b",()));
+         assert_eq!(closest_element_before("bb", &elements).unwrap(), ("b",()));
+         assert_eq!(closest_element_before("e", &elements).unwrap(), ("e",()));
+         assert_eq!(closest_element_before("ee",&elements).unwrap(), ("e",()));
+         assert_eq!(closest_element_before("i", &elements).unwrap(), ("i",()));
+         assert_eq!(closest_element_before("ii",&elements).unwrap(), ("i",()));
+    }
+
+    #[test]
+    fn test_encode_decode() {
+        let input = sample_string(1,1000);
+        let encoded = encode(input.as_bytes()).unwrap();
+        let mut reader: BufReader<&[u8]> = BufReader::new(encoded.as_slice());
+        let decoded = decode(&mut reader).unwrap();
+
+        assert_eq!(input, str::from_utf8(decoded.as_slice()).unwrap());
+    }
+
+    #[test]
+    fn test_random() {
+        let file_path = "test_temp.seg";
+        let state = random_state(100);
+
+        let segment = SegmentStore::create_from_iterator(
+            file_path.to_string(), 
+            state.iter().map(|(k, v)| (k.to_string(), v.to_string()))
+        
+        ).unwrap();
+
+        for (k, v) in state {
+            let result = segment.get(&k).unwrap();
+            assert_eq!(result.unwrap(), v.to_string());
+        }
+
+        let _ = fs::remove_file(file_path);
+    }
+
+    fn random_state(count: u32) -> BTreeMap<String, String> {
+        let mut entries = BTreeMap::new();
+        for _ in 0..count {
+            entries.insert(sample_string(1, 10), sample_string(1, 20));
+        }
+        
+        entries
+    }
+
+    fn sample_string(min: usize, max: usize) -> String {        
+        let mut entropy = rand::thread_rng();
+        let length: usize = entropy.gen_range(min..max);
+
+        entropy.sample_iter(&Alphanumeric)
+                .take(length)
+                .map(char::from)
+                .collect()
+    }
 }
