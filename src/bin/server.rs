64981673bbd0c00176398e6ea3a9155b8a006370
zawie <zawie@rice.edu>
2024-07-14 14:15:47 -0700

    ❗️Robust error handling in server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,5 @@
 use lib::{database::*, Storage};
-use log::{debug, info, trace};
-use std::fs;
+use log::{debug, info};
 use std::net::TcpStream;
 use std::{net::TcpListener, path::PathBuf};
 use std::io::{prelude::*, BufReader};
@@ -27,40 +26,51 @@ fn handle_connection(mut stream: TcpStream, db: &mut Database) {
         .take_while(|line| !line.is_empty())
         .collect();
     
-    println!("Request: {http_request:#?}");
+    debug!("Request: {http_request:#?}");
 
     let key = http_request[0].split_whitespace().nth(1).unwrap().split("/").nth(1).unwrap();
 
-    let response = if http_request[0].starts_with("GET") {
-        print!("GET request for key: {key}");
+    let (status, contents) = if http_request[0].starts_with("GET") {
+        info!("GET request for key: {key}");
         let contents = db.get(key).unwrap();
         match contents {
             Some(contents) => {
-                let status_line = "HTTP/1.1 200 OK";
-                let length = contents.len();
-                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}")
+                (200, contents)
             }
             None => {
-                String::from("HTTP/1.1 404\r\nContent-Length: 0\r\n\r\n")
+                (404, String::from("Key not found!"))
             }
         }
     } else if http_request[0].starts_with("POST") {
-        println!("POST request for key: {key}");
+        info!("POST request for key: {key}");
         let content_length = http_request[http_request.len() - 1].split_whitespace().nth(1).unwrap();
         let content_length = content_length.parse::<usize>().unwrap();
-        println!("Content-Length: {content_length}");
+        debug!("Content-Length: {content_length}");
 
         let mut buf = vec![0u8; content_length];
-        buf_reader.read_exact(&mut buf).unwrap();
-        db.set(key, &String::from_utf8(buf).unwrap()).unwrap();
-
-        String::from("HTTP/1.1 200\r\nContent-Length: 0\r\n\r\n")
+        if buf_reader.read_exact(&mut buf).is_ok() {
+            match String::from_utf8(buf) {
+                Ok(value) => {
+                    if db.set(key, value.as_str()).is_ok() {
+                        (201, value)
+                    } else {
+                        (500, String::from("Oops! Something went wrong."))
+                    }                }
+                Err(e) => {
+                    (400, format!("Unable to read content: {e}"))
+                }
+            }
+        } else {
+            (400, String::from("Unable to read content"))
+        }
     } else {
-        String::from("HTTP/1.1 405\r\nContent-Length: 0\r\n\r\n")
+        (405, String::from("Invalid method"))
     };
 
+    let len = contents.len();
+    let response = format!("HTTP/1.1 {status}\r\nContent-Length: {len}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 
-    println!("Response: {:#?}", response);
+    info!("Response: {:#?}", response);
 
 }
